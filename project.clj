e9fc708b2917508d81204136aaf0291d0d56674e
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-28 13:06:27 +0200

    move cljc files under src/cljc and test/cljc

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject tamagotchi "0.0.1-SNAPSHOT"
   :description "tamagotchi library"
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  :source-paths ["src/cljc"]
+  :test-paths ["test/cljc"]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}
              ;; You can add dependencies that apply to `lein midje` below.
              ;; An example would be changing the logging destination for test runs.
