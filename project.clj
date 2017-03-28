8dfc12e6a196ce283f1a4ad7402c0b147eccd899
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-28 13:07:50 +0200

    bump up clojure and midje dependencies and declare lein-midje plugin too

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject tamagotchi "0.0.1-SNAPSHOT"
   :description "tamagotchi library"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :source-paths ["src/cljc"]
   :test-paths ["test/cljc"]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]]}
              ;; You can add dependencies that apply to `lein midje` below.
              ;; An example would be changing the logging destination for test runs.
              :midje {}})
